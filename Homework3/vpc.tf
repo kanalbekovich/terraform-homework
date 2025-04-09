9d2bd4b760c22cf00fecb24257feaa8b74722fb3
kanalbekovich <kanalbekovich@gmail.com>
2025-04-09 05:16:43 +0000

    modified homework3 files

--- Homework3/vpc.tf
+++ Homework3/vpc.tf
@@ -74,11 +74,6 @@ resource "aws_route_table" "public" {
 resource "aws_route_table" "private" {
   vpc_id = aws_vpc.main.id
 
-  route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.gw.id
-  }
-
   tags = {
     Name = "private-rt"
   }
